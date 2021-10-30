668ddcf3256c9972ca375bb0c6e018dc71170ffc
Juliano Cesar Magoga <49631397+JulianoCesarM@users.noreply.github.com>
2021-10-29 21:30:43 -0300

    Foi colocado ":2" para adiciona mais um espaço, para o valor ficar alinhado

--- Aula2/Tabuada.py
+++ Aula2/Tabuada.py
@@ -2,4 +2,4 @@ valor = int(input('Digite o valor da tabuada: '))
 print('Tabuada do número: {}'.format(valor))
 for i in range(11):
     res = valor*i
-    print('{} x {} = {}'.format(valor, i, res))
+    print('{} x {:2} = {}'.format(valor, i, res))
